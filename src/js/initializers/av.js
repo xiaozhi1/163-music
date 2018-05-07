916f93885a36d1fcb4c70575e56757d8718e1935
xiaozhi1 <qasxed5@163.com>
2018-05-07 15:01:00 +0800

    MVC 和 新建歌曲

---
+++ src/js/initializers/av.js
@@ -0,0 +1,9 @@
+{
+    let APP_ID = 'HMtHtmOXfbjkblsXFTzTbfwR-gzGzoHsz';
+    let APP_KEY = 'PdBs25a4pxOcXl1eJSnwUI2F';
+    
+    AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+    });
+}
