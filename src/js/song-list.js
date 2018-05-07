db7464207bd542ccf33603fd9bc54384b7cbcd2b
xiaozhi1 <qasxed5@163.com>
2018-05-07 16:35:38 +0800

    点击歌曲时激活歌曲项

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -10,12 +10,17 @@
             $el.html(this.template)
             let {songs} = data
             let liList = songs.map((song)=>
-                $('<li></li>').text(song.name)
+                $('<li></li>').text(song.name).attr('data-song-id',song.id)
             )
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
+        },
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active').removeClass('active')
         },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
@@ -29,7 +34,7 @@
             var query = new AV.Query('Song');
             return query.find().then((songs)=>{
                 this.data.songs = songs.map((song)=>{
-                   return {id: song.id, ...song.attributes}    
+                    return {id: song.id, ...song.attributes}
                 })
                 return songs
             })
@@ -40,16 +45,28 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
+            this.bindEvents()
+            this.getAllSongs()
+        },
+        getAllSongs(){
+            return  this.model.find().then(()=>{
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select',{id:songId})
+            })
+        },
+        bindEventHub(){
             window.eventHub.on('upload',()=>{
                 this.view.clearActive()
             })
             window.eventHub.on('create',(songData)=>{
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
-            })
-            this.model.find().then(()=>{
-                console.log('------',this.model.data)
-                this.view.render(this.model.data)    
             })
         }
     }
